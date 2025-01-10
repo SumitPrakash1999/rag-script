25825836777ca52ec78ee25e6d2aaaecf8773b24
Sumit Prakash <sumit.prakash@students.iiit.ac.in>
2025-01-10 15:23:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -316,4 +316,4 @@ async def ask_question(query: QueryRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8081)))
